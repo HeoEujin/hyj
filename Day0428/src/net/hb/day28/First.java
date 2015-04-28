355854e35dbe538fd8b415dc21c32d51b0d2e777
USER <USER@USER-PC>
2015-04-28 12:41:04 +0900

    launching..

---
+++ Day0428/src/net/hb/day28/First.java
@@ -0,0 +1,20 @@
+package net.hb.day28;
+public class First {
+	public static void main(String[] args) {
+		System.out.println("First.java");
+		String name = "홍길동";		//이름
+		String addr = "시청";		//주소
+		int age = 1;						//나이
+		
+		System.out.println("이름 = "+name);
+		age=27;
+		System.out.println("나이 = "+age);
+		System.out.println("주소 = "+addr);
+		
+		System.out.println("\n영수증출력 10:03 10:06");
+		
+		
+
+	}//main end
+
+}//class END
---
+++ Day0428/src/net/hb/day28/HYJ.java
@@ -0,0 +1,9 @@
+package net.hb.day28;
+
+public class HYJ {
+	public static void main(String[] args) {
+		System.out.println("HYJ.java");
+		System.out.println("이름=허유진");
+		System.out.println("\n영수증출력 11:00");
+	}//main end
+}//class END
